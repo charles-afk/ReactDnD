dc6b2cd4f30a6323a27befb85903fd0885d934b3
charles-afk <agudelo.charles@gmail.com>
2022-07-31 00:02:14 -0400

    update

--- drag-n-drop/src/Modal.jsx
+++ drag-n-drop/src/Modal.jsx
@@ -1,29 +1,69 @@
-import React, {useState} from 'react'
+import React, {Fragment, useState} from 'react';
+import './Modal.css';
 
 const Modal = ({tabs}) => {
-    const [mainTabs, setMainTabs] = useState();
 
-    let main = {}
-    let extras = {}
+    const [toggle, setToggle] = useState(true);
+    const [edit, setEdit] = useState(false);
+
+    let main = {};
+    let extras = {};
     for(const tabName in tabs) {
         if(tabs[tabName].extra === false) {
-            main[tabName] = tabs[tabName]
+            main[tabName] = tabs[tabName];
         } else {
-            extras[tabName] = tabs[tabName]
+            extras[tabName] = tabs[tabName];
         }
     }
     
-    const mainTabsJsx = Object.entries(main).forEach(([key, value]) => {
-        console.log(key,value)
-        return <div>
-            {key}
-        </div>
-    })
+    const mainTabsJsx = <Fragment>
+        <div className='mainTab'>twitter</div>
+        <div className='mainTab'>facebook</div>
+        <div className='mainTab'>youtube</div>
+        <div className='mainTab'>linkedin</div>
+    </Fragment>;
 
-    console.log(mainTabsJsx)
+    const extraTabsJsx = <Fragment>
+        <div className='extraTab'>instagram</div>
+        <div className='extraTab'>snapchat</div>
+        <div className='extraTab'>pinterest</div>
+    </Fragment>;
+
+    const editJsx = <Fragment>
+        {edit ? 'Update' : 'Edit +'}
+    </Fragment>
+
+    const extraToggle = (toggle) => {
+        let elems = document.getElementsByClassName('extraTab');
+        if(toggle === true) {
+            for (let i=0;i<elems.length;i+=1){
+                elems[i].style.display = 'block';
+            }
+            setToggle(false)
+        } else {
+            for (let i=0;i<elems.length;i+=1){
+                elems[i].style.display = 'none';
+            }
+            setToggle(true)
+        }
+    }
+
     return (
         <div className='Modal'>
-            {mainTabsJsx}
+            <div className='mainContainer'>   
+                {mainTabsJsx}
+            </div>
+            <div className="extraContainer">
+                <div className='elips' onClick={()=>extraToggle(toggle)}>...</div>
+                <ttp>
+                    <extras>
+                        {extraTabsJsx}
+                    </extras>
+                </ttp>
+            </div>
+            <div className='edit' onClick={()=>setEdit(!edit)}>
+                {editJsx}
+            </div>
         </div>
     )
 }
