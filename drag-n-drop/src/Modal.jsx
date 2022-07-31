1951aa5d661b63fe189ffcc7777523d5800bbead
charles-afk <agudelo.charles@gmail.com>
2022-07-31 18:45:16 -0400

    working drag and drop

--- drag-n-drop/src/Modal.jsx
+++ drag-n-drop/src/Modal.jsx
@@ -17,16 +17,16 @@ const Modal = ({tabs}) => {
     }
     
     const mainTabsJsx = <Fragment>
-        <div className='mainTab' draggable="true">twitter</div>
-        <div className='mainTab' draggable="true">facebook</div>
-        <div className='mainTab' draggable="true">youtube</div>
-        <div className='mainTab' draggable="true">linkedin</div>
+        <div className='mainTab draggable' draggable="true">twitter</div>
+        <div className='mainTab draggable' draggable="true">facebook</div>
+        <div className='mainTab draggable' draggable="true">youtube</div>
+        <div className='mainTab draggable' draggable="true">linkedin</div>
     </Fragment>;
 
     const extraTabsJsx = <Fragment>
-        <div className='extraTab'>instagram</div>
-        <div className='extraTab'>snapchat</div>
-        <div className='extraTab'>pinterest</div>
+        <div className='extraTab draggable' draggable="true">instagram</div>
+        <div className='extraTab draggable' draggable="true">snapchat</div>
+        <div className='extraTab draggable' draggable="true">pinterest</div>
     </Fragment>;
 
     const editJsx = <Fragment>
@@ -48,42 +48,154 @@ const Modal = ({tabs}) => {
         }
     }
 
-    const dragStart = (e) => {
-        e.dataTransfer.setData("text/html", e.target.id);
-    }
+    // const draggableElems = document.querySelectorAll(".draggable");
+    // const droppableElem = document.querySelectorAll(".droppable");
 
-    const dragOver = (e) => {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = "move";
-    }
+    // draggableElems.forEach(elem => {
+    //     elem.addEventListener("dragstart", dragStart); 
+    // })
 
-    const drop = (e) => {
-        e.preventDefault();
-        const data = e.dataTransfer.getData("text/html");
-        e.target.appendChild(document.getElementById(data));
-    }
+    // droppableElem.forEach(elem => {
+    //     elem.addEventListener("dragenter", dragEnter); 
+    //     elem.addEventListener("dragover", dragOver); 
+    //     elem.addEventListener("dragleave", dragLeave); 
+    //     elem.addEventListener("drop", drop); 
+    // });
 
-    window.addEventListener('DOMContentLoaded', () => {
-        const element = document.getElementsByClassName("mainTab");
-        element.addEventListener("dragstart", dragStart);
-      });
+    // function dragStart(event) {
+    //     event.dataTransfer.setData("text", event.target.id); 
+    // }
+      
+    // function dragEnter(event) {
+    //     if(!event.target.classList.contains("dropped")) {
+    //         event.target.classList.add("droppable-hover");
+    //     }
+    // }
+    
+    // function dragOver(event) {
+    //     if(!event.target.classList.contains("dropped")) {
+    //         event.preventDefault(); 
+    //     }
+    // }
+    
+    // function dragLeave(event) {
+    //     if(!event.target.classList.contains("dropped")) {
+    //         event.target.classList.remove("droppable-hover");
+    //     }
+    // }
+    
+    // function drop(event) {
+    // event.preventDefault(); 
+    // event.target.classList.remove("droppable-hover");
+    // const draggableElementData = event.dataTransfer.getData("text"); 
+    // const droppableElementData = event.target.getAttribute("data-draggable-id");
+    // const isCorrectMatching = draggableElementData === droppableElementData;
+    //     if(isCorrectMatching) {
+    //         const draggableElement = document.getElementById(draggableElementData);
+    //         event.target.classList.add("dropped");
+    //         event.target.style.backgroundColor = window.getComputedStyle(draggableElement).color;
+    //         draggableElement.classList.add("dragged");
+    //         draggableElement.setAttribute("draggable", "false");
+    //         event.target.insertAdjacentHTML("afterbegin", `<div className='mainTab draggable'></div>`);
+    //     }
+    // }
+
+
+    // const dragStart = (e) => {
+    //     e.dataTransfer.setData("text/html", e.target.id);
+    // }
+
+    // const dragOver = (e) => {
+    //     // e.preventDefault();
+    //     // e.dataTransfer.dropEffect = "move";
+    //     if(!e.target.classList.contains("dropped")) {
+    //         e.preventDefault(); // Prevent default to allow drop
+    //       }
+    // }
+
+    // function dragEnter(event) {
+    //     if(!event.target.classList.contains("dropped")) {
+    //       event.target.classList.add("droppable-hover");
+    //     }
+    // }
+
+    // function dragLeave(event) {
+    //     if(!event.target.classList.contains("dropped")) {
+    //       event.target.classList.remove("droppable-hover");
+    //     }
+    // }
+
+    // const drop = (e) => {
+    //     e.preventDefault();
+    //     const data = e.dataTransfer.getData("text/html");
+    //     e.target.appendChild(document.getElementById(data));
+    // }
+
+    // window.addEventListener('DOMContentLoaded', () => {
+    //     const element = document.getElementsByClassName("mainTab");
+    //     element.addEventListener("dragstart", dragStart);
+    //   });
+
+    const draggables = document.querySelectorAll('.draggable')
+    const containers = document.querySelectorAll('.container')
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add('dragging')
+        })
+      
+        draggable.addEventListener('dragend', () => {
+            draggable.classList.remove('dragging')
+        })
+    })
+      
+    containers.forEach(container => {
+        container.addEventListener('dragover', e => {
+            e.preventDefault()
+            const afterElement = getDragAfterElement(container, e.clientY)
+            const draggable = document.querySelector('.dragging')
+            if (afterElement == null) {
+                container.appendChild(draggable)
+            } else {
+                container.insertBefore(draggable, afterElement)
+            }
+        })
+    })
+      
+    function getDragAfterElement(container, y) {
+        const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+      
+        return draggableElements.reduce((closest, child) => {
+            const box = child.getBoundingClientRect()
+            const offset = y - box.top - box.height / 2
+            if (offset < 0 && offset > closest.offset) {
+                return { offset: offset, element: child }
+            } else {
+                return closest
+            }
+        }, { offset: Number.NEGATIVE_INFINITY }).element
+    }
 
     return (
         <div className='Modal'>
-            <div className='mainContainer' id='target' onDrop={drop} onDragOver={dragOver}>   
+
+            <div className='mainContainer container' id='target'>   
                 {mainTabsJsx}
             </div>
+
             <div className="extraContainer">
                 <div className='elips' onClick={()=>extraToggle(toggle)}>...</div>
                 <ttp>
-                    <extras>
+                    <extras className='droppable container'>
                         {extraTabsJsx}
                     </extras>
                 </ttp>
             </div>
+
             <div className='edit' onClick={()=>setEdit(!edit)}>
                 {editJsx}
             </div>
+
         </div>
     )
 }
